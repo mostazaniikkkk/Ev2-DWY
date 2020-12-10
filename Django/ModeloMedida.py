c74b4e842cb90dafa00151930780f9ada98280f9
Sergio Valle <mostazaniikkkk@users.noreply.github.com>
2020-12-09 23:58:12 -0300

    Final assault

--- Django/ModeloMedida.py
+++ Django/ModeloMedida.py
@@ -9,6 +9,22 @@ class ModeloMedida():
     habitaciones = 0
     segundoPiso = False
     ventanas = False
-    coste = 0
     cocina = 1
+
+    if ventanas == True:
+        costeVentanas = 250000
+    else:
+        costeVentanas = 0
+
+    if living == True:
+        costeLiving = 500000
+    else:
+        costeLiving = 0
+
+    if comedor == True:
+        costeComedor = 500000
+    else:
+        costeComedor = 0
+
+    coste = (baños*750000) + (dormitorios*500000) + costeVentanas + costeLiving + costeComedor + 500000
     casa = CasaPrefab(baños, dormitorios, habitaciones, living, comedor, segundoPiso, ventanas, cocina, coste)
